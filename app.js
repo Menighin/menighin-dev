91a3018c71ae9ad0bef1428bf0cda9d2b4d856b8
João Menighin <joao.menighin@gmail.com>
2019-03-02 17:30:53 -0300

    Binding the correct port

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path')
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     res.send('Hello menighin-dev.');
 });
 
-app.listen(3000, function () {
-    console.log('Listening on port 3000!');
-  });
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
