ca4e9387afecb7350c356e38c8320699918edf9d
Menighin <joao.menighin@gmail.com>
2026-04-12 22:59:14 -0300

    Right path

--- vite.config.js
+++ vite.config.js
@@ -13,24 +13,8 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: 'src/index.html',
-                "comida-di-buteco": 'src/cdb.html',
+                "comida-di-buteco": 'src/comida-di-buteco/index.html',
             },
         },
-    },
-    // Plugin customizado para interceptar a URL no ambiente de Local Dev
-    plugins: [
-        {
-            name: 'dev-server-rewrite',
-            configureServer(server) {
-                server.middlewares.use((req, res, next) => {
-                    // Quando o usuario acessar a url literal /comida-di-boteco
-                    if (req.url === '/comida-di-buteco') {
-                        // O Vite internamente servirá nosso arquivo src/cdb.html
-                        req.url = '/cdb.html';
-                    }
-                    next();
-                });
-            }
-        }
-    ]
+    }
 });
